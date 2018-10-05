3463d370fd2d227ed02b85d38dbd8db4cb438470
Daniela Sayuri Yassuda <danielayassuda@gmail.com>
2018-10-05 14:06:36 -0300

    Add React Router and create routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/products">Produtos</Link>
+            </li>
+            <li>
+              <Link to="/products/new">Adicionar um novo produto</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Redirect
+              from="/"
+              exact={true}
+              to="/products"
+            />
+            <Route
+              path="/products"
+              exact={true}
+              render={() => <p>products</p>}
+            />
+            <Route
+              path="/products/new"
+              exact={true}
+              render={() => <p>new product</p>}
+            />
+            <Route render={() => <p>404</p>} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
